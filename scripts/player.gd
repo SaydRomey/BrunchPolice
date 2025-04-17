258ff4397e4c17c4ecd2c6c1da54cacf8765535b
Sayd Romey <sayd@null.net>
2025-04-17 18:59:10 -0400

    rolling implemented

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,38 +1,38 @@
 extends CharacterBody2D
 
-#https://www.youtube.com/watch?v=93QBVvCzUGI&list=PLhXFaKLHQJdXpwaNt6gGwpHLTWL0m-TSL&index=11
-
+# Constants
 @export var speed = 150
 @export var gravity = 20
 @export var jump_force = -500
+@export var roll_speed = 200
+@export var double_tap_time = 0.3
 
+# Nodes
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
 @onready var cshape = $CollisionShape2D
-
-var is_crouching = false
-var stuck_under_object = false
+@onready var coyote_timer = $CoyoteTimer
+@onready var jump_buffer_timer = $JumpBufferTimer
+@onready var jump_height_timer = $JumpHeightTimer
+@onready var roll_timer = $RollTimer
 @onready var crouch_raycast1 = $CrouchRaycast_1
 @onready var crouch_raycast2 = $CrouchRaycast_2
 
-var can_coyote_jump = false
-@onready var coyote_timer = $CoyoteTimer
+# Collision shapes
+var standing_cshape = preload("res://ressources/knight_standing_cshape.tres")
+var crouching_cshape = preload("res://ressources/knight_crouching_cshape.tres")
 
-var jump_buffered = false
-@onready var jump_buffer_timer = $JumpBufferTimer
-
-@onready var jump_height_timer = $JumpHeightTimer
-
-var is_rolling = false
+# Movement vars
 var roll_direction = 0
-@onready var roll_timer = $RollTimer
-@export var roll_speed = 200
-@export var double_tap_time = 0.3
 var last_tap_left_time = 0.1
 var last_tap_right_time = 0.1
 
-var standing_cshape = preload("res://ressources/knight_standing_cshape.tres")
-var crouching_cshape = preload("res://ressources/knight_crouching_cshape.tres")
+# Flags
+var is_crouching = false
+var stuck_under_object = false
+var can_coyote_jump = false
+var jump_buffered = false
+var is_rolling = false
 
 #func _process(delta):
 	#print()
@@ -45,7 +45,7 @@ func _physics_process(delta):
 		if velocity.y > 1000:
 			velocity.y = 1000
 		
-#	Handle double tap for roll
+#	Double tap to roll
 	var current_time = Time.get_ticks_msec() / 1000.0
 	
 	if Input.is_action_just_pressed("move_left"):
@@ -58,33 +58,30 @@ func _physics_process(delta):
 			start_roll(1)
 		last_tap_right_time = current_time
 	
-#	Get movement direction
+#	Horizontal movement
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
 	
-#	Movement logic
 	if is_rolling:
 		velocity.x = roll_speed * roll_direction
 	else:
 		velocity.x = speed * horizontal_direction
-	
 		if horizontal_direction != 0:
 			switch_direction(horizontal_direction)
 	
-		#	Handle jump
-		if Input.is_action_just_pressed("jump"):
-			jump_height_timer.start()
-			jump()
+	#	Jump
+	if Input.is_action_just_pressed("jump"):
+		jump_height_timer.start()
+		jump()
 	
-#	Crouch logic
+#	Crouch
 	if Input.is_action_just_pressed("crouch"):
 		crouch()
 	elif Input.is_action_just_released("crouch"):
 		if above_head_is_empty():
 			stand()
-		else:
-			if stuck_under_object != true:
-				stuck_under_object = true
-				#print("Player stuck, setting stuck_under_object to true")
+		elif !stuck_under_object:
+			stuck_under_object = true
+			#print("Player stuck, setting stuck_under_object to true")
 	
 	if stuck_under_object && above_head_is_empty():
 		if !Input.is_action_pressed("crouch"):
@@ -92,22 +89,22 @@ func _physics_process(delta):
 			stuck_under_object = false
 			#print("Player was stuck, now standing straight")
 	
-#	Move and handle landing/fall
+#	Move
 	var was_on_floor = is_on_floor()
 	move_and_slide()
 	
-#	Started to fall
+#	Fall detection
 	if was_on_floor && !is_on_floor() && velocity.y >= 0:
 		#print("Fall")
 		can_coyote_jump = true
 		coyote_timer.start()
 	
-#	Touched ground
+#	Landing detection
 	if !was_on_floor && is_on_floor():
 		#print("Touched ground")
 		if jump_buffered:
 			jump_buffered = false
-			print("Buffered jump")
+			#print("Buffered jump")
 			jump()
 	
 	update_animations(horizontal_direction)
@@ -115,11 +112,13 @@ func _physics_process(delta):
 func jump():
 	if is_rolling:
 		return
+	if !above_head_is_empty():
+		return
 	if is_on_floor() || can_coyote_jump:
 		velocity.y = jump_force
 		if can_coyote_jump:
 			can_coyote_jump = false
-			print("Coyote jump")
+			#print("Coyote jump")
 	else:
 		if !jump_buffered:
 			jump_buffered = true
@@ -130,33 +129,33 @@ func start_roll(direction: int):
 		return
 	is_rolling = true
 	roll_direction = direction
-	
-	print("Rolling ", "left" if direction == -1 else "right")
-	
+	#print("Rolling ", "left" if direction == -1 else "right")
 	ap.play("roll")
 	roll_timer.start()
 	cshape.shape = crouching_cshape
 	cshape.position.y = 0
 
 func crouch():
-	if is_rolling:
-		return
 	if is_crouching:
 		return
 	is_crouching = true
 	cshape.shape = crouching_cshape
 	cshape.position.y = 0
-	
+
 func stand():
-	if is_rolling:
-		return
 	if is_crouching == false:
 		return
-	#print("Standing up")
-	is_crouching = false
-	cshape.shape = standing_cshape
-	cshape.position.y = -5
-	
+	if !above_head_is_empty():
+		stuck_under_object = true
+		print("Can't stand: object above")
+		return
+	else:
+		#print("Standing up")
+		is_crouching = false
+		stuck_under_object = false
+		cshape.shape = standing_cshape
+		cshape.position.y = -5
+
 func switch_direction(horizontal_direction):
 	sprite.flip_h = (horizontal_direction == -1)
 	sprite.position.x = horizontal_direction * 4
@@ -171,22 +170,19 @@ func _on_jump_height_timer_timeout() -> void:
 	if !Input.is_action_pressed("jump"):
 		if velocity.y < -100:
 			velocity.y = -10
-			print("Low jump")
-	else:
-		print("High jump")
+			#print("Low jump")
+	#else:
+		#print("High jump")
 
 func _on_roll_timer_timeout() -> void:
 	is_rolling = false
 	if above_head_is_empty():
-		if !is_crouching:
-			cshape.shape = standing_cshape
-			cshape.position.y = -5
+		stand()
+		stuck_under_object = false
 	else:
 #		Not enough space, stay crouched
-		is_crouching = true
+		crouch()
 		stuck_under_object = true
-		cshape.shape = crouching_cshape
-		cshape.position.y = 0
 
 func above_head_is_empty() -> bool:
 	var result = !crouch_raycast1.is_colliding() && !crouch_raycast2.is_colliding()
